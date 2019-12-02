13e29e38ab5ef6e6c40aa888718cb62f03510c7b
Jon Nordby <jononor@gmail.com>
2019-12-02 15:25:24 +0100

    mlflow: Support Heroku Postgresql

--- mlflow.sh
+++ mlflow.sh
@@ -7,4 +7,11 @@ if [[ -z "${ARTIFACT_URL}" ]]; then
     export ARTIFACT_URL="mlruns"
 fi
 
+if [[ -z "${DATABASE_URL}" ]]; then
+    export DATABASE_URL="./mlruns"
+fi
+
+# Fix naming convention mismatch between Heroku and mlflow
+DATABASE_URL=${DATABASE_URL//postgres:/postgresql:} 
+
 exec mlflow server --backend-store-uri=$DATABASE_URL --default-artifact-root=$ARTIFACT_URL
