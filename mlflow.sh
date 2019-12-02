1bb54ec6fd21bd04c76b9e63ece3108d0d02866a
Jon Nordby <jononor@gmail.com>
2019-12-02 15:12:07 +0100

    mlflow: Add default ARTIFACT_URL

--- mlflow.sh
+++ mlflow.sh
@@ -2,4 +2,9 @@
 
 # FIXME: write google credentials to disk from envvar
 
-exec mlflow server --backend-store-uri $DATABASE_URL --default-artifact-root $ARTIFACT_URL
+
+if [[ -z "${ARTIFACT_URL}" ]]; then
+    export ARTIFACT_URL="mlruns"
+fi
+
+exec mlflow server --backend-store-uri=$DATABASE_URL --default-artifact-root=$ARTIFACT_URL
